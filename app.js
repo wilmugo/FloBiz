4c9ae9e25d3b8738d1d7b23b25e20498e0f17352
Wilfredo Murillo <wilmugo@gmail.com>
2023-03-13 11:51:55 -0400

    config prismic cms and change module type in package

--- app.js
+++ app.js
@@ -1,20 +1,62 @@
-const express = require('express');
+import path from 'path';
+import express from 'express';
+import { fileURLToPath } from 'url';
+import * as prismic from '@prismicio/client';
+import * as PrismicH from '@prismicio/helpers';
+import { client } from './prismicConfig.js';
+//const express = require('express');
+//const app = express();
+//const path = require('path');
 const app = express();
-const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+//const PrismicH = require('@prismicio/helpers');
+//const { client } = require('./prismicConfig.js');
+
+/*const initApi = (req) => {
+  return Prismic.createClient(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOkEN,
+    req,
+    fetch,
+  });
+};
+
+const handleLinkResolver = (doc) => {
+  //if (doc.type === 'page') {
+  //  return '/page/' + doc.uid;
+  //} else if (doc.type === 'blog_post') {
+  //  return '/blog/' + doc.uid;
+  //}
+  return '/';
+};
+*/
+//Meta content fetch
+const { results } = await client.getByType('meta');
+const [meta] = results;
+
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    PrismicH,
+  };
+  next();
+});
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-  res.render('pages/home');
+app.get('/', async (req, res) => {
+  //const { results } = await client.get()
+  const { results } = await client.getByType('home');
+  const [home] = results;
+  res.render('pages/home', { home, meta });
 });
 
-app.get('/about', (req, res) => {
+app.get('/about', async (req, res) => {
   res.render('pages/about');
 });
 
-app.get('/detail/:id', (req, res) => {
+app.get('/detail/:uid', (req, res) => {
   res.render('pages/detail');
 });
 
