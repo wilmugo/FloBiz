5f43fbe4bde7b5084ef6afdfd5a6612d0d4956b1
Wilfredo Murillo <wilmugo@gmail.com>
2023-03-20 22:11:51 -0400

    add sass scafolding

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.use((req, res, next) => {
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', async (req, res) => {
   //const { results } = await client.get()
